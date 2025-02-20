817f4483d6ae4741ddcc4ae76d1f815ff10a660e
Emmanuel Onu <sireconu@gmail.com>
2025-02-20 03:09:31 +0100

    Modified Custom css library

--- script.js
+++ script.js
@@ -1,40 +1,65 @@
-let copyContainer = document.querySelector("#show-hide-container");
-
-let welcome = document.querySelector(".welcome");
-let css = document.querySelector(".css");
-let cssButton = document.querySelector(".css-button");
-cssButton.onclick = function showCssPage() {
-  welcome.classList.add("welcome", "welcome2");
-  copyContainer.classList.replace("copy-container", "copy-container2");
-  css.classList.remove("hidden");
-};
-
 document.addEventListener("DOMContentLoaded", () => {
   let span1 = document.querySelector(".animate-welcome1");
   let span2 = document.querySelector(".animate-welcome2");
   let span3 = document.querySelector(".animate-welcome3");
-
-  let cssButton = document.querySelector(".css-button");
   setInterval(() => {
     span1.classList.toggle("animate__fadeInDownBig");
     span2.classList.toggle("animate__rotateIn");
     span3.classList.toggle("animate__fadeInUpBig");
-    welcome.classList.toggle("bg");
   }, 5000);
 });
 
-//copy text function
-let button = document.querySelector(".copy-btn");
-button.addEventListener("click", () => {
-  let textToCopy = document.querySelector(".text-to-copy").innerHTML;
-  // Copy text to the clipboard
-  navigator.clipboard.writeText(textToCopy);
-  button.innerHTML = "Copied";
-   
- 
-    copyContainer.classList.replace("copy-container2", "copy-container");
-    setTimeout(() => {
-       css.classList.add("hidden");
-       welcome.classList.replace("welcome2", "welcome");
-    }, 4000);
+let tutorialLinks = document.querySelector(".tutorial-courses");
+let tutorialButton = document.querySelector(".tutorial-button");
+tutorialButton.addEventListener("click", () => {
+  tutorialLinks.classList.toggle("height-10");
+   weAreSocial.classList.add("hidden");
+});
+
+let courses = document.querySelectorAll(".course");
+for (const course of courses) {
+  course.onclick = () => tutorialLinks.classList.toggle("height-10", false);
+}
+
+let smartCopy = document.querySelector(".smart-copy");
+let weAreSocial = document.querySelector(".we-are-social");
+let yesButton = document.querySelector(".yes");
+yesButton.onclick = () => {
+  let copied = document.querySelector(".copied");
+  let customCSS = document.querySelector(".custom-css-class2").innerHTML;
+  navigator.clipboard.writeText(customCSS);
+  copied.innerHTML = "Copied";
+  setTimeout(() => {
+    cssPage.classList.add("hidden");
+    welcome.classList.replace("hidden", "show-welcome");
+    smartCopy.classList.remove("height-20");
+    weAreSocial.classList.remove("hidden");
+  }, 3000);
+};
+
+let noButton = document.querySelector(".no");
+noButton.onclick = () => {
+  setTimeout(() => {
+    cssPage.classList.add("hidden");
+    welcome.classList.replace("hidden", "show-welcome");
+    smartCopy.classList.remove("height-20");
+     weAreSocial.classList.remove("hidden");
+  }, 2000);
+};
+
+let welcome = document.querySelector(".welcome");
+let customCssButton = document.querySelector(".custom-css");
+let cssPage = document.querySelector(".css-page");
+customCssButton.addEventListener("click", () => {
+  welcome.classList.replace("show-welcome", "hidden");
+  cssPage.classList.remove("hidden");
+  setTimeout(() => {
+    smartCopy.classList.add("height-20");
+  }, 3000);
+});
+
+let exampleButton = document.querySelector(".example-button");
+exampleButton.addEventListener("click", () => {
+  let example = document.querySelector(".example-css").innerHTML;
+  navigator.clipboard.writeText(example);
 });
